a0cbd88350d3b9c0c2864a789f99528c38a43b8b
Jongmin Byun <cyberzen.byun@gmail.com>
2015-11-26 21:54:03 +0900

    major update: main
    add analyze area-slope curve according to the fixed vertical interval

--- main.m
+++ main.m
@@ -3,7 +3,7 @@ function main
 % @brief Function to extract stream longitudinal profiles from unfilled
 % DEMs
 %
-% @version 0.4.5 / 2015-11-26
+% @version 0.5.0 / 2015-11-26
 % @author Jongmin Byun
 %==========================================================================
 
@@ -14,7 +14,7 @@ clear all
 % Constants
 INPUT_DIR = '../data/input';
 OUTPUT_DIR = '../data/output';
-DEMFileName = 'n37_e128_1arc_v3.tif';
+DEMFileName = 'tareaR50m.tif';
 DEMFilePath = fullfile(INPUT_DIR,DEMFileName);
 [DEM,R] = geotiffread(DEMFilePath);
 % note that R means MapCellsReference or GeographicPostingsReference
@@ -41,34 +41,37 @@ end
 
 DEM = double(DEM);
 
-% % for the MATLAB without MappingToolbox
-% dataFileName = 'a_load_DEM_2015-11-25.mat';
-% dataFilePath = fullfile(INPUT_DIR,dataFileName);
-% load(dataFilePath);
+nanMask = (DEM == 32767 | DEM == -9999); % avoid null values of DEM
+nanMask(DEM == 0) = true; % include 0 as null value
+orgDEM = DEM; % original DEM
+DEM(nanMask) = nan;
 
 figure(1); clf;
 imagesc(DEM);
 set(gca,'DataAspectRatio',[1 1 1]);
 colorbar;
 title('Digital Elevation Model');
+DEM(nanMask) = orgDEM(nanMask);
 
-%% Check and remove flat cells in DEM based on smoothing
+% Check and remove flat cells in DEM based on smoothing
 
-% % for debug
-% DEM = orgDEM;
+% % for the MATLAB without MappingToolbox
+% dataFileName = 'a_load_DEM_2015-11-26.mat';
+% dataFilePath = fullfile(INPUT_DIR,dataFileName);
+% load(dataFilePath);
 
 % identify flat cells in DEM
 
 % assign flow directions to the DEM using D8 algorithm
+minElev = min(DEM(~nanMask));
+DEM(nanMask) = minElev - 1; % for the calculation of the model boundary
 [steepestDescentSlope,slopeAllNbr,SDSFlowDirection,SDSNbrY,SDSNbrX] ...
     = CalcSDSFlow(DEM,dX,dY);
+DEM(nanMask) = minElev + 1;
 % make a map of flat cells
-nanMask = (DEM == 32767 | DEM == -9999); % avoid null values of DEM
-nanMask(DEM == 0) = true; % include 0 as null value
 flatRegMap = ProcessFlat(DEM,~nanMask,slopeAllNbr);
 
 % add small random elevation values to DEM
-orgDEM = DEM; % original DEM
 randE = rand(mRows,nCols);
 DEM = DEM + randE * 0.00001;
 
@@ -83,7 +86,10 @@ for i = 1:fSize
 end
 h = 1/sum(h(:)) * h;
 % smooth using the filter
-DEM = filter2(h,DEM);
+nSmooth = 1;
+for i=1:nSmooth
+    DEM = filter2(h,DEM);
+end
 
 % display difference between the original and smoothed dEM
 diffDEM = orgDEM - DEM;
@@ -97,6 +103,7 @@ colorbar;
 title('Flat cells in DEM');
 
 subplot(1,2,2);
+diffDEM(nanMask) = nan;
 imagesc(diffDEM(fSize*2:end-fSize*2,fSize*2:end-fSize*2));
 set(gca,'DataAspectRatio',[1 1 1]);
 colorbar;
@@ -133,16 +140,17 @@ end
 % first, include the area out of nanMask
 targetDrainage = (~nanMask); % target drainage
 
-IS_BND_LOWER = true;
+IS_BND_LOWER = false;
 
-if IS_BND_LOWER == false
+if IS_BND_LOWER == true
     
     DEM(~targetDrainage) = min(DEM(targetDrainage)) - 0.1;
     
-else % IS_BND_HIGHER
+else % IS_BND_LOWER == false
 
     % pick the outlet of the target drainage
 
+    DEM(~targetDrainage) = inf;
     % extract the boundary of the target drainage
     s = strel('square',3); % structural element when eroding image
     dilatedTarget = imerode(targetDrainage,s); 
@@ -186,7 +194,6 @@ end
     = ProcessSink(DEM,targetDrainage,slopeAllNbr ...
         ,SDSNbrY,SDSNbrX,SDSFlowDirection);
 
-    
 % frequency distribution of the types of sub-flooded region's outlet
 figure(4); clf;
 set(gcf, 'Color',[1,1,1]);
@@ -223,7 +230,7 @@ colorbar;
 set(gca,'DataAspectRatio',[1 1 1]);
 title('Sub-flooded Region Outlet');
 
-% Modify flow direction of the cells within each depression
+%% Modify flow direction of the cells within each depression
 
 % Assign flow direction to the cells in each depression region
 [m2SDSNbrY,m2SDSNbrX ... % flow direction modified cells along the path to each regional minima
@@ -239,12 +246,12 @@ disp(subFldRegTree.tostring);
 
 %% Calculate upstream cells number of all cells within every depression
 
-% for debug
-clear all
-INPUT_DIR = '../data/input';
-dataFileName = 'a_CalcUpstreamCells_2015-11-20_2.mat';
-dataFilePath = fullfile(INPUT_DIR,dataFileName);
-load(dataFilePath);
+% % for debug
+% clear all
+% INPUT_DIR = '../data/input';
+% dataFileName = 'a_CalcUpstreamCells_2015-11-20_2.mat';
+% dataFilePath = fullfile(INPUT_DIR,dataFileName);
+% load(dataFilePath);
 
 % A. Calculate the number of upstream cells out of flooded region
 nUpstreamCellsWithFldReg = CalcUpstreamCellsWithFldReg(DEM,targetDrainage ...
@@ -300,11 +307,11 @@ colorbar
 % % for debug
 % clear all
 % INPUT_DIR = '../data/input';
-% dataFileName = 'a_CalcUpstreamCells_2015-11-18.mat';
+% dataFileName = 'a_CalcUpstreamCells_2015-11-26.mat';
 % dataFilePath = fullfile(INPUT_DIR,dataFileName);
 % load(dataFilePath);
 
-figure(2); clf;
+figure(9); clf;
 set(gcf, 'Color',[1,1,1]);
 
 imagesc(DEM);
@@ -314,16 +321,16 @@ title('Digital Elevation Model');
 
 % identify stream path using given initial and end point coordinates
 initY = 617; initX = 719;
-endY = 852; endX = 299;
+endY = 853; endX = 297;
 [streamPath,distFromInit] = RecordStrPath(initY,initX,endY,endX ...
     ,mRows,nCols,m2SDSNbrY,m2SDSNbrX,dY,dX);
 
 % stream longitudinal profile using raw DEM
-elev = DEM(streamPath(:));
+elev = orgDEM(streamPath(:));
 nCells = numel(streamPath);
 
 % draw figure
-figure(9); clf;
+figure(10); clf;
 set(gcf, 'Color',[1,1,1]);
 
 subplot(3,1,1)
@@ -336,9 +343,9 @@ ylabel('Elevation [m]')
 xlim([0 distFromInit(end)])
 ylim([min(elev),max(elev)])
 
-% smoothing stream profiles
+%% smoothing stream profiles
 
-considerNbrForElev = [50,150]; % number of considering neighbor cells
+considerNbrForElev = [25,50,75,100]; % number of considering neighbor cells
 % note that you can compare the differences due to the number of
 % considering neighbor cells
 nCNbr = numel(considerNbrForElev); % number of cases
@@ -360,9 +367,9 @@ ylabel('Elevation [m]')
 xlim([0 distFromInit(end)])
 ylim([min(elev),max(elev)])
 
-% draw stream gradient
+%% draw stream gradient
 
-considerNbrForSlope = 45;
+considerNbrForSlope = [25,50,75,100];
 chosenSizeForSlope = 1;
 nCNbrForSlope = numel(considerNbrForSlope);
 
@@ -384,7 +391,7 @@ ylabel('Slope')
 xlim([0 distFromInit(end)])
 ylim([min(slopePerDist(:,1)),max(slopePerDist(:,1))])
 
-% draw corrected upstream area profiles on the interesting stream paths
+%% draw corrected upstream area profiles on the interesting stream paths
 
 % choose a stream gradient profile
 chosenSlopeForUpArea_Slope = 1;
@@ -422,7 +429,7 @@ for ithCell = 2:nCells
 
 end
 
-figure(12); clf;
+figure(11); clf;
 set(gcf, 'Color',[1,1,1]);
 
 subplot(2,1,1)
@@ -449,7 +456,7 @@ xlabel('Upstream Area [m^2]')
 ylabel('Slope')
 xlim([upstreamAreaProf(1),upstreamAreaProf(end)])
 
-% export stream gradient profile to ArcGIS
+%% export stream gradient profile to ArcGIS
 
 fileName = strcat(num2str(initY),'_',num2str(initX),'_',num2str(endY),'_',num2str(endX),'_profile_gradient');
 
@@ -461,4 +468,142 @@ end
 IPDFDFilePath = fullfile('/Users/cyberzen/Dropbox/temp',fileName);
 ExportRasterAsArcGrid(IPDFDFilePath,ithProfileDistMap,R.DeltaX ...
     ,R.XLimWorld(1,1),R.YLimWorld(1,1));
+
+%% Analyze slope-area relationship according to fixed vertical interval
+% note: errors in DEM produce many depressions and negative slope values
+% along a stream profile. Therefore, this algorithm concentrates on
+% making a stream profile without depression. Using this stream profile,
+% it calculates new slope values and log binnined average slope values.
+% However using a smoothed stream proile as an input data makes analysis
+% better. Because the smoothed stream profile has less depressions than
+% raw stream profile data.
+
+% make elevation values with fixed vertical interval.
+% note: if you want to see subtle change in relatively flat region, lower
+% contour interval
+contInterval = 1; % contour interval
+% fixed vertical interval
+ipElevMin = 10 * floor(0.1 * elev(end));
+ipElevMax = 10 * ceil(0.1 * elev(1));
+ipElev = ipElevMax:-contInterval:ipElevMin;
+
+% interpolated distance values according to fixed vertical interval
+tStrProfElev = elev;
+% remove depressions in an input stream profile.
+nCells = numel(distFromInit);
+for a = nCells:-1:2
+    if tStrProfElev(a-1) <= tStrProfElev(a)
+        tStrProfElev(a-1) = tStrProfElev(a) + 0.000001;
+    end
+end
+ipDistFromInit = interp1(tStrProfElev,distFromInit,ipElev);
+
+% draw new stream profile with both elevation with fixed vertical interval
+% and interpolated distance.
+
+figure(12);
+subplot(5,1,1)
+% for debug
+% plot(distFromInit,tStrProfElev,'o',ipDistFromInit,ipElev);
+plot(ipDistFromInit,ipElev);
+
+% grid on
+title('Longitudinal Stream Profile')
+xlabel('Distance From Divide [m]')
+ylabel('Elevation [m]')
+xlim([0 max(ipDistFromInit)])
+ylim([min(ipElev),max(ipElev)])
+
+% calculate new slope values using interpolated elevation and distance data
+% note: basically new slope means local slope rather than 'trend slope'.
+nCells2 = numel(ipElev);
+newSlope = zeros(nCells2,1);
+for a = 1
+    newSlope(a) = (ipElev(a) - ipElev(a+1)) ...
+        / (ipDistFromInit(a+1) - ipDistFromInit(a));
+end
+for a = 2:nCells2-1
+    newSlope(a) = (ipElev(a-1) - ipElev(a+1)) ...
+        / (ipDistFromInit(a+1) - ipDistFromInit(a-1));
+end
+for a = nCells2
+    newSlope(a) = (ipElev(a-1) - ipElev(a)) ...
+        / (ipDistFromInit(a) - ipDistFromInit(a-1));
+end
+
+% draw newly calculated slope vs distance relationship
+subplot(5,1,2)
+plot(ipDistFromInit,newSlope,'*')
+
+% grid on
+xlabel('Distance From Initiaion [m]')
+ylabel('Slope')
+xlim([0 max(ipDistFromInit)])
+ylim([min(newSlope),max(newSlope)])
+
+% draw newly calculated upstream area vs elevation relationship
+ipUpstreamArea = interp1(tStrProfElev,upstreamAreaProf,ipElev);
+
+subplot(5,1,3)
+plot(ipDistFromInit,ipUpstreamArea);
+
+set(gca,'YScale','log');
+
+% grid on
+title('Upstream Area Profile')
+xlabel('Distance From Divide [m]')
+ylabel('Upstream Area [m^2]')
+xlim([0 max(ipDistFromInit)])
+ylim([min(ipUpstreamArea),max(ipUpstreamArea)])
+
+% draw newly calculated upstream area vs slope relationship
+subplot(5,1,4)
+scatter(ipUpstreamArea,newSlope,'m+')
+
+set(gca,'XScale','log','YScale','log');
+% grid on
+title('Upstream Area - Slope')
+xlabel('Upstream Area [m^2]')
+ylabel('Slope')
+xlim([min(ipUpstreamArea),max(ipUpstreamArea)])
+
+% determine logarithmic binned average slopes
+
+logBinSize = 0.1;
+
+minPower = floor(log10(min(ipUpstreamArea))/logBinSize) * logBinSize;
+maxPower = ceil(log10(max(ipUpstreamArea))/logBinSize) * logBinSize;
+diffPower = maxPower - minPower;
+
+cellsNo3 = floor(diffPower/logBinSize);
+
+tempArea = zeros(1,cellsNo3);
+tempSlope = zeros(1,cellsNo3);
+
+for a = 1:cellsNo3
+    
+    logAreaMin = minPower + (a-1) * logBinSize;
+    logAreaMax = logAreaMin + logBinSize;
+    
+    tempArea(a) = 10^(logAreaMin + logBinSize * 0.5);
+    
+    j = find(ipUpstreamArea >= 10^logAreaMin ...
+        & ipUpstreamArea < 10^logAreaMax);
+    k = size(nonzeros(j));
+    if k(1) > 0
+        tempSlope(a) = 10^(mean(log10(newSlope(j))));
+    end
+end
+
+% extract non-zero elements
+lbarea = tempArea(find(tempSlope));
+lbslope = tempSlope(find(tempSlope));
+
+subplot(5,1,5)
+loglog(lbarea,lbslope,'rs','MarkerFaceColor','r','MarkerSize',3)
+% grid on
+title('Upstream Area - Slope')
+xlabel('Upstream Area [m^2]')
+ylabel('Slope')
+xlim([min(ipUpstreamArea),max(ipUpstreamArea)])
 
